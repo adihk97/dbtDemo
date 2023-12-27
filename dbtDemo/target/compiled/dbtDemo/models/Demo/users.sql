e34916d216d356f93918f5dc12f0013c7e56ad6d
adihk97 <37631492+adihk97@users.noreply.github.com>
2023-12-27 22:37:27 +0530

    Delete dbtDemo directory

--- dbtDemo/target/compiled/dbtDemo/models/Demo/users.sql
+++
@@ -1,5 +0,0 @@
-with data as (
-    select User_Id, Location, Location_2, Age from user 
-) 
-
-select * from data
--- dbtDemo/target/compiled/dbtDemo/models/Demo/users_dbt.sql
+++
@@ -1,5 +0,0 @@
-with data as (
-    select User_Id, Location, Location_2, Age from dbtDemo.user 
-) 
-
-select * from data
