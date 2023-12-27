65252fc3395306998707fdd596c5c9295ef84e72
adihk97 <37631492+adihk97@users.noreply.github.com>
2023-12-27 21:29:23 +0530

    Add files via upload

---
+++ dbtDemo/models/Demo/users_dbt.sql
@@ -0,0 +1,5 @@
+with data as (
+    select User_Id, Location, Location_2, Age from dbtDemo.user 
+) 
+
+select * from data
---
+++ dbtDemo/target/compiled/dbtDemo/models/Demo/schema.yml/not_null_tourism_place_name.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select place_name
+from `dbtdemo-409319`.`dbtDemo`.`tourism`
+where place_name is null
+
+
