2cbac2f8d31442ce38ab81f88f43d3fa7bfb68ee
adihk97 <37631492+adihk97@users.noreply.github.com>
2023-12-27 22:04:40 +0530

    Add files via upload

---
+++ dbtDemo/target/compiled/dbtDemo/models/Demo/Ratings.sql
@@ -0,0 +1,5 @@
+with data as (
+    select User_Id, Place_Id, Place_Ratings from Ratings
+)
+
+select * from data
---
+++ dbtDemo/target/compiled/dbtDemo/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `dbtdemo-409319`.`dbtDemo`.`my_second_dbt_model`
+where id is null
+
+
