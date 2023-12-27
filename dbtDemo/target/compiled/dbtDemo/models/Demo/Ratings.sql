d51aa9d8bf596f72a6f6d5a88ca5082f2052f7dc
adihk97 <37631492+adihk97@users.noreply.github.com>
2023-12-27 21:56:48 +0530

    Delete dbtDemo directory

--- dbtDemo/target/compiled/dbtDemo/models/Demo/Ratings.sql
+++
@@ -1,5 +0,0 @@
-with data as (
-    select User_Id, Place_Id, Place_Ratings from Ratings
-)
-
-select * from data
--- dbtDemo/target/compiled/dbtDemo/models/Demo/tourism.sql
+++
@@ -1,5 +0,0 @@
-with data as (
-    select Place_Id, Place_Name, Category, City, Price, Rating, Time_minutes, Coordinate, Lat, Long from Tourism
-) 
-
-select * from data
